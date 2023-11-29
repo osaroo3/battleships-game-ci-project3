0fda6435abe9ae54f25ed6ef2c06d14820e94e97
osaroo3 <>
2023-11-29 03:51:53 +0000

    Add function new game and  prompt the user to set the grid size

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+from pprint import pprint
+
+
+scores = {
+    "computer_score" : 0,
+    "player_score" : 0
+}
+
+
+grid_size = 5
+#print( " Number of ships: 4")
+#print("-----------------------------------")
+
+   
+def new_game():
+    """
+    Starts the game with board populated, while setting both computer and player score to zero
+    
+    """
+
+    print("-----------------------------------") 
+    print( " Welcome to our BATTLESHIPS GAME!")
+    print("-----------------------------------")
+
+size = input("Please enter board size e.g it must be 4 or 5 \n")
+grid_size = size
+print(f"size is {grid_size}")
+
+new_game()
