25badc44d9fe6a56a7e11af8e5d4863266145bb3
osaroo3 <>
2023-12-01 04:22:15 +0000

    Validate user input to set the grid or board size

--- run.py
+++ run.py
@@ -6,13 +6,42 @@ scores = {
     "computer_score" : 0,
     "player_score" : 0
 }
-
-
 grid_size = 5
-#print( " Number of ships: 4")
-#print("-----------------------------------")
 
-   
+def g_size():
+    """
+    set the grid(board) size
+    """
+    while True:
+        print("Please set your grid(board) size.")
+    
+        data = input("Enter a number here:\n")
+        validate_g_size(data)
+
+        if validate_g_size(data):
+            print("Number is valid!")
+            break
+
+    return data   
+
+def validate_g_size(value):
+    """
+    Validate user input to set the grid(board) size
+    """
+    try:
+        grid_type =[4,5]
+        value = int(value)
+        if value not in grid_type :
+            raise ValueError(
+                f"Number 4 or 5 is required, you provided {value}"
+            )
+        
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
+
+
 def new_game():
     """
     Starts the game with board populated, while setting both computer and player score to zero
@@ -20,11 +49,12 @@ def new_game():
     """
 
     print("-----------------------------------") 
-    print( " Welcome to our BATTLESHIPS GAME!")
+    print( "Welcome to our BATTLESHIPS GAME!")
+    print("-----------------------------------")
+    grid_size = g_size()
+    print( "Number of ships: 4")
+    print(f"Grid size(board): {grid_size}")
     print("-----------------------------------")
 
-size = input("Please enter board size e.g it must be 4 or 5 \n")
-grid_size = size
-print(f"size is {grid_size}")
 
 new_game()
