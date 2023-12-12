1b755f92f58012bafea7389950fd2b141c4795a5
osaroo3 <>
2023-12-12 19:47:26 +0000

    Use PEP8 standard to format code

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import random
 
 scores = {
-    "computer_score" : 0,
-    "player_score" : 0
+    "computer_score": 0,
+    "player_score": 0
 }
 
-grid_size = 5
+grid_size = 6
 player_ship_coordinate = []
 player_guess_coordinate = []
 comp_ship_coordinate = []
@@ -18,56 +18,56 @@ def g_size():
     """
     while True:
         print("Please set your grid(board) size.")
-    
         data = input("Enter a number here:\n")
         validate_g_size(data)
 
         if validate_g_size(data):
             print("Number is valid!")
             break
+    return data
 
-    return data   
 
 def validate_g_size(value):
     """
     Validate user input to set the grid(board) size
     """
     try:
-        grid_type =[6,7,8]
+        grid_type = [6, 7, 8]
         value = int(value)
-        if value not in grid_type :
+        if value not in grid_type:
             raise ValueError(
                 f"Number 6,7 or 8 is required, you provided {value}"
             )
-        
+
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
     return True
 
-def validate_input(coord="row",size=""):
+
+def validate_input(coord="row", size=""):
     """
-    validate player inputs coordinates 
+    validate player inputs coordinates
     """
     while True:
         if coord != "row":
             data = input("Enter a column : \n")
-            validate_coordinates(data,size)
-            if validate_coordinates(data,size):
+            validate_coordinates(data, size)
+            if validate_coordinates(data, size):
                 print("Number is valid!")
                 break
 
         else:
-            data = input("Enter a row : \n") 
-            validate_coordinates(data,size)
-            if validate_coordinates(data,size):
+            data = data = input("Enter a row : \n")
+            validate_coordinates(data, size)
+            if validate_coordinates(data, size):
                 print("Number is valid!")
                 break
 
-    return data 
+    return data
 
 
-def validate_coordinates(value,size):
+def validate_coordinates(value, size):
     """
     Validates input coordinates to ensure the are within board grid
     """
@@ -80,7 +80,7 @@ def validate_coordinates(value,size):
             raise ValueError(
                 f"Number must be 0 or {size-1} , you provided {value}"
             )
-        
+
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
@@ -91,17 +91,17 @@ def ran_one(size):
     """
     Random number between 0 and size-1
     """
-    return random.randint(0,size-1)
+    return random.randint(0, size-1)
 
 
 def ran_two(size):
     """
     Random number between 0 and size-1
     """
-    return random.randint(0,size-1)
+    return random.randint(0, size-1)
 
 
-def add_ships(board,ships,player_type="player"):
+def add_ships(board, ships, player_type="player"):
     """
     Add ships to board
     """
@@ -110,21 +110,21 @@ def add_ships(board,ships,player_type="player"):
             a = ran_one(grid_size)
             b = ran_two(grid_size)
             board[a][b] = "#"
-            comp_ship_coordinate.append((a,b))
+            comp_ship_coordinate.append((a, b))
     else:
         for n in range(ships):
             x = ran_one(grid_size)
             y = ran_two(grid_size)
             board[x][y] = "@"
-            player_ship_coordinate.append((x,y))
-                   
+            player_ship_coordinate.append((x, y))
 
-def game_board(board,ships,player_type=""): 
+
+def game_board(board, ships, player_type=""):
     """
     Prints board(player or computer) with ships appended
     """
-    add_ships(board,ships,player_type)
-    
+    add_ships(board, ships, player_type)
+
     for r in board:
         g = (" ".join(r))
         print(g)
@@ -140,58 +140,60 @@ def count_ship_hits(board):
     return count
 
 
-def player_turn(board,turn,size):
+def player_turn(board, turn, size):
     """
-    Function handles player turn, appends guesses to player guess list and computer board
+    Function handles player turn,
+    appends guesses to player guess list and computer board
     """
     print("\nPlayer's turn")
     row = int(validate_input("row", size))
     column = int(validate_input("column", size))
-    print(f"You entered row:{row} column:{column}") 
+    print(f"You entered row: {row} column: {column}")
 
-    if (row,column) not in comp_ship_coordinate:
+    if (row, column) not in comp_ship_coordinate:
         print("You missed!")
-        player_guess_coordinate.append((row,column))
+        player_guess_coordinate.append((row, column))
         board[row][column] = "X"
 
     else:
         print("You hit!")
-        player_guess_coordinate.append((row,column))
+        player_guess_coordinate.append((row, column))
         board[row][column] = "*"
         scores["player_score"] = count_ship_hits(board)
         score = scores["player_score"]
         print(f"Player score: {score}")
-        
-        
-def computer_turn(board,turn,size):
+
+
+def computer_turn(board, turn, size):
     """
-    Function handles computer turn, appends guesses to computer guess list and player board
+    Function handles computer turn,
+    appends guesses to computer guess list and player board
     """
     print("\nComputer's turn")
-    row = random.randint(0,size-1)
-    column =  random.randint(0,size-1)
-    print(f"Computer entered row:{row} column:{column}") 
-    if (row,column) not in player_ship_coordinate:
+    row = random.randint(0, size-1)
+    column = random.randint(0, size-1)
+    print(f"Computer entered row:{row} column:{column}")
+    if (row, column) not in player_ship_coordinate:
         print("Computer missed!")
-        comp_guess_coordinate.append((row,column))
+        comp_guess_coordinate.append((row, column))
         board[row][column] = "X"
     else:
         print("Computer hit!")
-        comp_guess_coordinate.append((row,column))
+        comp_guess_coordinate.append((row, column))
         board[row][column] = "*"
         scores["computer_score"] = count_ship_hits(board)
         score = scores["computer_score"]
         print(f"Computer score: {score}")
 
-  
+
 def turns(board, turn="", size=""):
     """
     Handles both player and computer turns
     """
     if turn != "player_turn":
-        computer_turn(board, "computer_turn",size)
+        computer_turn(board, "computer_turn", size)
     else:
-        player_turn(board, "player_turn",size) 
+        player_turn(board, "player_turn", size)
 
 
 def print_board(board):
@@ -202,12 +204,13 @@ def print_board(board):
 
 def new_game():
     """
-    Starts the game with board populated, while setting both computer and player score to zero
-    
+    Starts the game with board populated,
+    while setting both computer and player score to zero
+
     """
 
-    print("-----------------------------------") 
-    print( "Welcome to our BATTLESHIPS GAME!")
+    print("-----------------------------------")
+    print("Welcome to our BATTLESHIPS GAME!")
     print("-----------------------------------")
     grid_size = int(g_size())
     print(f"Grid size(board): {grid_size}")
@@ -217,41 +220,34 @@ def new_game():
 
     # Game board without ships
     player_board = [["." for i in range(grid_size)] for j in range(grid_size)]
-    computer_board = [["." for i in range(grid_size)] for j in range(grid_size)]
-    
-    print("-----------------------------------")   
+    computer_board = [["." for i in range(grid_size)]for j in range(grid_size)]
+
+    print("-----------------------------------")
     name = input("Enter your name here:\n")
     print(f"{name}'s board")
-    game_board(player_board, num_ships,"player")
-    print(player_ship_coordinate)
+    game_board(player_board, num_ships, "player")
     print(" ")
     print(f"Computer's board")
-    game_board(computer_board, num_ships,"computer")
-    print(comp_ship_coordinate)
-
-
+    game_board(computer_board, num_ships, "computer")
     while True:
         # Player turn
-        turns(computer_board, "player_turn", grid_size )
+        turns(computer_board, "player_turn", grid_size)
         print("Computer's board")
         print_board(computer_board)
-        
         if count_ship_hits(computer_board) == 4:
-            print("-----------------------------------") 
+            print("-----------------------------------")
             print("Player wins!")
-            print("-----------------------------------") 
+            print("-----------------------------------")
             break
         # Computer turn
-        turns(player_board, "computer_turn", grid_size )
+        turns(player_board, "computer_turn", grid_size)
         print("Player's board")
         print_board(player_board)
-        
         if count_ship_hits(player_board) == 4:
-            print("-----------------------------------") 
+            print("-----------------------------------")
             print("Computer wins!")
-            print("-----------------------------------") 
+            print("-----------------------------------")
             break
-        
     print(f"{name}'s board")
     print_board(player_board)
     print(" ")
@@ -260,8 +256,8 @@ def new_game():
     PLAYER_SCORE = count_ship_hits(player_board)
     COMPUTER_SCORE = count_ship_hits(computer_board)
     print(f"\n{name}'s score: {COMPUTER_SCORE} Computer score: {PLAYER_SCORE}")
-    print("-----------------------------------") 
-    print( "THANK YOU FOR PLAYING!")
+    print("-----------------------------------")
+    print("THANK YOU FOR PLAYING!")
     print("-----------------------------------")
 
 
